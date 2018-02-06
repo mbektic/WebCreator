5e884c7a7fca1b775946cd1c89b73ee39ed9397c
Warren Gates <warrenckhan@gmail.com>
2018-02-06 17:58:10 -0500

    Updated GrapeJS w/ our Contaner-fluid box, 2 columns, and 3 columns, added jumbotron and icons to their respective buttons   updated grape-js config file with local bootstrap and jquery file.
    Signed-off-by: Warren Gates <warrenckhan@gmail.com>

--- js/grapesJS-config.js
+++ js/grapesJS-config.js
@@ -1,3 +1,4 @@
+// MAIN CONFIG
 var editor = grapesjs.init({
     height: '100%',
     showDevices:true,
@@ -9,12 +10,13 @@ var editor = grapesjs.init({
     components: '<div style="min-height: 100px" class="container-fluid"></div>',
     canvas: {
         styles: [
-            'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'
+            './css/bootstrap.css',
+            './css/style.css'
         ],
         scripts: [
-            'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-            'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
-            'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js'
+            './js/jquery-3.3.1.min.js',
+            './js/popper.min.js',
+            './js/bootstrap.js'
         ]
     },
     // MANAGES PLUGINs
@@ -35,26 +37,26 @@ var editor = grapesjs.init({
         }
     }
 });
+
 // ADDS TOOLS TO SIDEBAR
 var blockManager = editor.BlockManager;
-// 'my-first-block' is the ID of the block
 blockManager.add('carousel', {
     category: "Web Serv Tools",
     label: 'Carousel',
-    content: '<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">'+
+    content: '<div id="carouselExampleIndicators" style="min-height: 100px;" class="carousel slide" data-ride="carousel" >'+
     '<ol class="carousel-indicators">'+
     '<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>'+'' +
     '<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>'+
     '<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>'+
     '</ol>'+
-    '<div class="carousel-inner">'+
-    '<div class="carousel-item active">'+
+    '<div style="min-height: 100px;" class="carousel-inner">'+
+    '<div style="min-height: 100px;" class="carousel-item active">'+
     '<img class="d-block w-100" src="" alt="First slide">' +
     '</div>' +
-    '<div class="carousel-item">' +
+    '<div style="min-height: 100px;" class="carousel-item">' +
     '<img class="d-block w-100" src="" alt="Second slide">' +
     '</div>'+
-    '<div class="carousel-item">' +
+    '<div style="min-height: 100px;" class="carousel-item">' +
     '<img class="d-block w-100" src="" alt="Third slide">' +
     '</div>' +
     '</div>' +
@@ -71,14 +73,52 @@ blockManager.add('carousel', {
 blockManager.add('container', {
     category: "Web Serv Tools",
     label: 'Container',
-    content: '<div style="min-height: 100px" class="container-fluid"></div>'
+    content: '<div style="min-height: 100px" class="container-fluid"></div>',
+    attributes: {
+        class: "gjs-fonts gjs-f-b1"
+    }
 });
 blockManager.add('row', {
     category: "Web Serv Tools",
     label: 'Row',
     content: '<div style="min-height: 100px" class="row"></div>'
 });
+blockManager.add('2 columns', {
+    category: "Web Serv Tools",
+    label: '2 Columns',
+    content: '<div class="row" style="min-height: 100px">'+
+    '<div class="col-sm" style="min-height: 100px">'+
+    '<p>Testing Text</p>'+
+    '</div>'+
+    '<div class="col-sm" style="min-height: 100px">'+
+    '<p>Testing Text</p>'+
+    '</div>'+
+    '</div>',
+    attributes: {
+        class: "gjs-fonts gjs-f-b2"
+    }
+});
+blockManager.add('3 columns', {
+    category: "Web Serv Tools",
+    label: '3 Columns',
+    content: '<div class="row" style="min-height: 100px">'+
+    '<div class="col-sm" style="min-height: 100px">'+
+    '<p>Testing Text</p>'+
+    '</div>'+
+    '<div class="col-sm" style="min-height: 100px">'+
+    '<p>Testing Text</p>'+
+    '</div>'+
+    '<div class="col-sm" style="min-height: 100px">'+
+    '<p>Testing Text</p>'+
+    '</div>'+
+    '</div>',
+    attributes: {
+        class: "gjs-fonts gjs-f-b3"
+    }
+});
 
+
+// STYLEMANAGER
 var styleManager = editor.StyleManager;
 
 styleManager.addSector('Dimension',{
@@ -92,5 +132,35 @@ styleManager.addSector('Dimension',{
     }]
 });
 
+// CUSTOM TEXTEDITOR
+var rte = editor.RichTextEditor;
+const action = [rte.remove('bold'),rte.remove('italic'),rte.remove('underline'),rte.remove('strikethrough'),rte.remove('link')];
 
+ rte.add('strong', {
+        icon: '<strong>B</strong>',
+        attributes: {title: 'Bold'},
+        result: rte => {
+            // rte.insertHTML(`<strong>${rte.selection()}</strong>`)
+            rte.exec('bold')
+
+        }
+});
+
+rte.add('italic',
+    {
+        icon: '<i>I</i>',
+        attributes: {title: 'Italics'},
+        result: rte => {
+            rte.insertHTML(`<i>${rte.selection()}</i>`);
+        }
+});
+
+rte.add('underline',
+    {
+        icon: '<u>U</u>',
+        attributes: {title: 'Underline'},
+        result: rte => {
+        rte.insertHTML(`<u>${rte.selection()}</u>`);
+    }
+});
 
